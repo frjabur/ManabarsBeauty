6cec13d57da0f20f6693883477a5f7251692754a
frjabur <frjabur@gmail.com>
2017-06-21 11:00:36 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -30,9 +30,9 @@ using Ensage.Common.Objects;
                     .ToList();
             foreach (var enemy in enemies)
             {
-                var start = HUDInfo.GetHPbarPosition(enemy) + new Vector2(1, HUDInfo.GetHpBarSizeY(enemy) - 7);
+                var start = HUDInfo.GetHPbarPosition(enemy) + new Vector2(1, HUDInfo.GetHpBarSizeY(enemy) - 9);
                 var manaperc = enemy.Mana / enemy.MaximumMana;
-                var size = new Vector2(HUDInfo.GetHPBarSizeX(), HUDInfo.GetHpBarSizeY() - 1);
+                var size = new Vector2(HUDInfo.GetHPBarSizeX(), HUDInfo.GetHpBarSizeY() - 6);
                 // Draw background
                 Drawing.DrawRect(start, size + new Vector2(1, 1), new Color(0, 0, 50, 150));
                 // Draw manabar
